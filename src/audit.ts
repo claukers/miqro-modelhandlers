7c28f7d93db59c766efd7158192e17a70060b599
claukers <claukers@github>
2021-08-24 08:22:55 -0400

    remove unused dep

--- src/audit.ts
+++ src/audit.ts
@@ -1,5 +1,4 @@
 import { ErrorHandler, Handler, Context, Logger } from "@miqro/core";
-import { loadSequelize } from "@miqro/database";
 import { DataTypes, Model, ModelCtor, Sequelize, Transaction } from "sequelize";
 
 const AuditModel = (auditModelName: string, sequelize: Sequelize): ModelCtor<Model<any>> => {
@@ -52,8 +51,8 @@ const auditLog = async (auditModel: ModelCtor<Model<any>>, ctx: Context, e?: Err
   }, transaction ? { transaction } : undefined);
 };
 
-export const AuditHandler = (auditModelName = "audit", sequelize?: Sequelize, logger?: Logger): Handler => {
-  const auditModel = AuditModel(auditModelName, sequelize ? sequelize : loadSequelize());
+export const AuditHandler = (auditModelName = "audit", sequelize: Sequelize, logger?: Logger): Handler => {
+  const auditModel = AuditModel(auditModelName, sequelize);
   auditModel.sync({
     force: false
   }).catch((e) => {
